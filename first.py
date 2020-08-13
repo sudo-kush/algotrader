38eb4d3de6ede827f4d173cea99d8610da39a87b
Caleb Menjivar <54721971+sudo-kush@users.noreply.github.com>
2020-08-13 13:28:03 -0700

    Add files via upload

--- first.py
+++ first.py
@@ -9,19 +9,23 @@ import numpy as np
 import matplotlib.pyplot as plt 
 import time 
 
-money = 20000
+money = 200000
 
 years = 3
 n = int(365*years)
-
+owned = 10
+inverse_owned = 10
 
 x = np.linspace(1,n,n)
 y = np.zeros(n)
+z = np.zeros(n)
 b = []
 account = [] 
 
 file1 = open('rip.csv','r')
+file2 = open('sqqq.csv', 'r')
 line = file1.readline()
+lineb = file2.readline()
 
 gain = 0
 loss = 0
@@ -32,6 +36,12 @@ for i in range(n):
     a = line2.split(',')
     b.append(a[0])
     y[i] = a[1]
+    
+# =============================================================================
+#     line2 = file2.readline()
+#     c = line2.split(',')
+#     z[i] = c[1]
+# =============================================================================
     
     #RSI
     #First Calculation 
@@ -63,22 +73,52 @@ for i in range(n):
             avggain = (prevgain *13 + currgain) / 14
             avgloss = (prevloss *13 + currloss) / 14            
             
-    RS = avggain / avgloss
-    RSI = 100 - 100 / (1+RS)       
+        RS = avggain / avgloss
+        RSI = 100 - 100 / (1+RS)    
+        
+        #Stuff to set up for actual trading 
+        leverage = 30 #percent 
+        maxmove = money*leverage/100   
+        
+        #Actual Trading 
+        if RSI < 30 and RSI > 0 and money > 0:
+            #move = maxmove//y[i]
+            multiple = 1
+            money = money - y[i]*multiple
+            account.append(money)
+            owned += multiple 
+            
+                 
+# =============================================================================
+#             if inverse_owned > 0:
+#                 inverse_shares = y[i]//z[i]
+#                 money = money + z[i]*inverse_shares
+#                 inverse_owned -= inverse_shares
+# =============================================================================
+     
+        
+            print('\n****** Bought',1.0,' Share ******\n')            
+            print('\nCurrent Money:',money,'\n')
+            
+        if RSI > 60 and owned > 0:
+            #move = maxmove//y[i]
+            multiple = 1
+            money = money + y[i]*multiple 
+            account.append(money)
+            owned -= multiple 
+            
+# =============================================================================
+#             inverse_shares = y[i]//z[i]
+#             money = money - z[i]*inverse_shares
+#             inverse_owned += inverse_shares
+# =============================================================================
+            
+            print('\n****** Sold',1.0,' Share ******\n')
+            print('\nCurrent Money:',money,'\n')
+            
     
-    if RSI < 30 and RSI > 0:
-        print('\n****** Bought Share ******\n')
-        money = money - y[i]
-        account.append(money)
-        print('\nCurrent Money:',money,'\n')
-    if RSI > 60:
-        print('\n****** Sold Share ******\n')
-        money = money + y[i] 
-        account.append(money)
-        print('\nCurrent Money:',money,'\n')
-    
-    print('Date:',b[i],'\nStock Price',y[i],'\nRSI',RSI,'\n')
-    time.sleep(0.5) 
+        print('Date:',b[i],'\nStock Price',y[i],'\nRSI',RSI,'\n')
+        #time.sleep(0.5) 
     
 trans = np.linspace(1,len(account),len(account))
     
